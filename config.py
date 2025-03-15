2ae7b2f9f1b7479baa27bf6f4053c2d0344b2e99
alya <aliaa23mahmoud@gmail.com>
2025-03-15 07:55:47 +0200

    edit admin id

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "7716381751:AAHZRjxloKPfq4nPxd1oP7fUWz22Xm7BtF0"
-ADMIN_ID = 5808711396  # Telegram ID of the bot admin
+ADMIN_ID =   1388747442# Telegram ID of the bot admin
 DB_CONFIG = {
     "host": "turntable.proxy.rlwy.net ",
     "user": "root",
