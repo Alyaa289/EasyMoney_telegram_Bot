c697dad4e9f50c55465cbd7625e8315672f97f97
alya <aliaa23mahmoud@gmail.com>
2025-03-15 07:18:04 +0200

    Initial commit - Telegram bot setup

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "7716381751:AAHZRjxloKPfq4nPxd1oP7fUWz22Xm7BtF0"
+ADMIN_ID = 5808711396  # Telegram ID of the bot admin
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "alya",
+    "password": "mumdad2002",
+    "database": "easymoneybot"
+}
