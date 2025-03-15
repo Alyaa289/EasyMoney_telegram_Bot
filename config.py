54b13d2f3f30d692712b378be972913eaac3d07c
alya <aliaa23mahmoud@gmail.com>
2025-03-15 07:38:51 +0200

    Added requirements.txt and Procfile

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 TOKEN = "7716381751:AAHZRjxloKPfq4nPxd1oP7fUWz22Xm7BtF0"
 ADMIN_ID = 5808711396  # Telegram ID of the bot admin
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "alya",
-    "password": "mumdad2002",
-    "database": "easymoneybot"
+    "host": "turntable.proxy.rlwy.net ",
+    "user": "root",
+    "password": "deumDYtgMcJbFKQgpVCIKYOyVclSpBQk",
+    "database": "railway"
+    
+    # "host": "localhost",
+    # "user": "alya",
+    # "password": "mumdad2002",
+    # "database": "easymoneybot"
 }
