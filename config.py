96502da50381ae664afe1a7f526bae30a4c751ee
Alyaa289 <87453383+Alyaa289@users.noreply.github.com>
2025-03-19 01:43:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-#TOKEN = "7716381751:AAHZRjxloKPfq4nPxd1oP7fUWz22Xm7BtF0"
-ADMIN_ID = 5808711396
-#1388747442# Telegram ID of the bot admin
+TOKEN = "7716381751:AAHZRjxloKPfq4nPxd1oP7fUWz22Xm7BtF0"
+ADMIN_ID = 1388747442# Telegram ID of the bot admin
 DB_CONFIG = {
     "host": "turntable.proxy.rlwy.net ",
     "user": "root",
